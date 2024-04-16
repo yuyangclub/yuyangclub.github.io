d4dc20c772b221b72fe7ba572a4c65c841d64e86
YuYang <zgl351351@gmailcom>
2024-04-17 02:06:33 +0800

    Site updated: 2024-04-17 02:06:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/15/hello-world/","2024/04/16/测试/"];function toRandomPost(){
+var posts=["2024/04/16/测试/","2024/04/15/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
